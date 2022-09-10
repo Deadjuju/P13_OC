a9fef59a2505a27f748ee4ac584b688e79a4f2be
Julien Garcia <julien.garcia-lp@laposte.net>
2022-09-10 14:09:05 +0200

    refactor(profiles): add an app name to urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from .views import profile, profiles_index
 
 
+app_name = "profiles"
+
 urlpatterns = [
-    path('profiles/', profiles_index, name='profiles_index'),
+    path('profiles/', profiles_index, name='index'),
     path('profiles/<str:username>/', profile, name='profile'),
 ]
