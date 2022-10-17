9be46eef52d3cb0ba5a8bbb6ec5e8d834e8fa7d0
Julien Garcia <julien.garcia-lp@laposte.net>
2022-10-17 19:21:33 +0200

    create settings package

--- oc_lettings_site/settings/settings.py
+++ oc_lettings_site/settings/settings.py
@@ -1,7 +1,7 @@
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -67,7 +67,7 @@ WSGI_APPLICATION = 'oc_lettings_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'oc-lettings-site.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../oc-lettings-site.sqlite3'),
     }
 }
 
