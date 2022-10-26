e91d51333e98230cbbe959cebbbd2ecd38914ebc
Julien Garcia <julien.garcia-lp@laposte.net>
2022-10-26 12:21:57 +0200

    setting up Sentry

--- oc_lettings_site/settings/production_settings.py
+++ oc_lettings_site/settings/production_settings.py
@@ -1,4 +1,6 @@
 import environ
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 from .base import *
 
@@ -14,6 +16,7 @@ environ.Env.read_env()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
+SENTRY_DSN = env('SENTRY_DSN')
 DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS += ["oc-p13.herokuapp.com"]
@@ -28,3 +31,19 @@ MIDDLEWARE.insert((index_SecurityMiddleware + 1), whitenoise_middleware)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+sentry_sdk.init(
+    dsn=SENTRY_DSN,
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
