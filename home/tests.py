6311a005d1d0ce98174f2075c6b76e49cae6b4e9
Julien Garcia <julien.garcia-lp@laposte.net>
2022-10-31 10:55:41 +0100

    change test for home app

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
+import pytest
 
-# Create your tests here.
+
+CLIENT = Client()
+
+
+@pytest.mark.django_db
+def test_lettings_route():
+    """
+    GIVEN a URL for index page,
+    WHEN a user tries to access this url",h
+    THEN template used and url name are correct.
+    """
+
+    url = reverse('home:index')
+    response = CLIENT.get(url)
+
+    assert url == "/"
+    assert response.status_code == 200
+
+    assert len(response.templates) == 1
+    template_used = response.templates[0].name
+    assert template_used == "index.html"
+
+    assert b"<title>Holiday Homes</title>" in response.content
